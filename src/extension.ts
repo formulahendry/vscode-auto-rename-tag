8ef86b9923344b117f36e703c83ce97eaecb7747
LaurentTreguier <laurent@treguier.org>
2016-07-20 19:41:11 +0200

    Remove console.log
    Is it necessary to keep this `console.log('Congratulations, your extension "auto-rename-tag" is now active!');` ?

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,6 @@ import { ExtensionContext } from 'vscode';
 import { TagManager } from './tagManager';
 
 export function activate(context: ExtensionContext) {
-    console.log('Congratulations, your extension "auto-rename-tag" is now active!');
-
     let tagManager = new TagManager();
     tagManager.run();
 }

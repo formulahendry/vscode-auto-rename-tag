aac45e0e07ff734617153f18f37e727fbb7a7498
Simon Siefke <simon.siefke@gmail.com>
2022-02-07 22:21:43 +0100

    update dependencies

--- packages/server/src/autoRenameTag.ts
+++ packages/server/src/autoRenameTag.ts
@@ -2,10 +2,10 @@ import { doAutoRenameTag } from 'service';
 import {
   RequestType,
   // TODO
-  TextDocument,
   TextDocuments,
-  VersionedTextDocumentIdentifier,
+  VersionedTextDocumentIdentifier
 } from 'vscode-languageserver';
+import { TextDocument } from 'vscode-languageserver-textdocument';
 
 interface Tag {
   readonly word: string;
@@ -25,46 +25,42 @@ interface Result {
   readonly originalOffset: number;
 }
 
-export const autoRenameTagRequestType = new RequestType<
-  Params,
-  Result[],
-  any,
-  any
->('$/auto-rename-tag');
+export const autoRenameTagRequestType = new RequestType<Params, Result[], any>(
+  '$/auto-rename-tag'
+);
 
 const NULL_AUTO_RENAME_TAG_RESULT: Result[] = [];
 
 export const autoRenameTag: (
   documents: TextDocuments<TextDocument>
-) => (params: Params) => Promise<Result[]> = (documents) => async ({
-  textDocument,
-  tags,
-}) => {
-  await new Promise((r) => setTimeout(r, 20));
-  const document = documents.get(textDocument.uri);
-  if (!document) {
-    return NULL_AUTO_RENAME_TAG_RESULT;
-  }
-  if (textDocument.version !== document.version) {
-    return NULL_AUTO_RENAME_TAG_RESULT;
-  }
-  const text = document.getText();
-  const results: Result[] = tags
-    .map((tag) => {
-      const result = doAutoRenameTag(
-        text,
-        tag.offset,
-        tag.word,
-        tag.oldWord,
-        document.languageId
-      );
-      if (!result) {
-        return result;
-      }
-      (result as any).originalOffset = tag.offset;
-      (result as any).originalWord = tag.word;
-      return result as Result;
-    })
-    .filter(Boolean) as Result[];
-  return results;
-};
+) => (params: Params) => Promise<Result[]> =
+  documents =>
+  async ({ textDocument, tags }) => {
+    await new Promise(r => setTimeout(r, 20));
+    const document = documents.get(textDocument.uri);
+    if (!document) {
+      return NULL_AUTO_RENAME_TAG_RESULT;
+    }
+    if (textDocument.version !== document.version) {
+      return NULL_AUTO_RENAME_TAG_RESULT;
+    }
+    const text = document.getText();
+    const results: Result[] = tags
+      .map(tag => {
+        const result = doAutoRenameTag(
+          text,
+          tag.offset,
+          tag.word,
+          tag.oldWord,
+          document.languageId
+        );
+        if (!result) {
+          return result;
+        }
+        (result as any).originalOffset = tag.offset;
+        (result as any).originalWord = tag.word;
+        return result as Result;
+      })
+      .filter(Boolean) as Result[];
+    return results;
+  };
